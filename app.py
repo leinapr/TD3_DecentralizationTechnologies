38ff20e3bff3ca21ca4aa5479a2148dd93bade1d
leinapr <162492449+leinapr@users.noreply.github.com>
2025-01-29 17:07:52 +0100

    Question 1

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ from flask import Flask, request, jsonify
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 
 # Initialize the Flask app
